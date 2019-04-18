69d2f8662c8ba1561a2511511846da1f154bbf95
Murat Celep <mcelep@redhat.com>
2019-04-18 13:02:36 +0200

    fix test

--- src/test/java/ch/appuio/techlab/Springboots2idemoApplicationTests.java
+++ src/test/java/ch/appuio/techlab/Springboots2idemoApplicationTests.java
@@ -2,12 +2,11 @@ package ch.appuio.techlab;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Springboots2idemoApplication.class)
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
 public class Springboots2idemoApplicationTests {
 
